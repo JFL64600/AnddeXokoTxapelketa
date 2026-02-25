0dee496c5ec52bc814e1843ed09009f84455b41a
Jean François LISSART <jeanfrancois.lissart@sdworx.com>
2026-02-25 17:55:05 +0100

    Sauvegarde d'un score

--- AnddeXokoTxapelketa/Controls/Show.xaml.cs
+++ AnddeXokoTxapelketa/Controls/Show.xaml.cs
@@ -2,6 +2,8 @@
 using AnddeXokoTxapelketa.EventsArgs;
 using AnddeXokoTxapelketa.Interfaces;
 using System.Configuration;
+using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -98,11 +100,75 @@ namespace AnddeXokoTxapelketa.Controls
         {
             if (!e.Canceled)
             {
-                ((Score)FindName($"ScoreR{e.Row}C{e.Column}")).Value = e.ScorePlayer1;
-                ((Score)FindName($"ScoreR{e.Column}C{e.Row}")).Value = e.ScorePlayer2;
-                (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[e.Row - 1].Results[e.Column - ((e.Row > e.Column) ? 1 : 2)] = e.ScorePlayer1;
-                (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[e.Column - 1].Results[e.Row - ((e.Column > e.Row) ? 1 : 2)] = e.ScorePlayer2;
-                Tools.SaveTournament(_root, ((Models.Tournament)_tournament));
+                if (_tournament is Models.Tournament)
+                {
+                    ((Score)FindName($"ScoreR{e.Row}C{e.Column}")).Value = e.ScorePlayer1;
+                    ((Score)FindName($"ScoreR{e.Column}C{e.Row}")).Value = e.ScorePlayer2;
+                    (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[e.Row - 1].Results[e.Column - ((e.Row > e.Column) ? 1 : 2)] = e.ScorePlayer1;
+                    (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[e.Column - 1].Results[e.Row - ((e.Column > e.Row) ? 1 : 2)] = e.ScorePlayer2;
+                    Tools.SaveTournament(_root, ((Models.Tournament)_tournament));
+                }
+                else if (_tournament is Models.New.Tournament)
+                {
+                    if (e.Row < e.Column)
+                    {
+                        ((Score)FindName($"ScoreR{e.Row}C{e.Column}")).Value = e.ScorePlayer1;
+                        ((Score)FindName($"ScoreR{e.Column}C{e.Row}")).Value = e.ScorePlayer2;
+                    }
+                    else
+                    {
+                        ((Score)FindName($"ScoreR{e.Row}C{e.Column}")).Value = e.ScorePlayer2;
+                        ((Score)FindName($"ScoreR{e.Column}C{e.Row}")).Value = e.ScorePlayer1;
+                    }
+                    foreach (Models.New.Rotation rotation in ((Models.New.Tournament)_tournament).Groups[_currentGroup].Rotations)
+                    {
+                        foreach (Models.New.Match match in rotation.Matches)
+                        {
+                            if (e.Row < e.Column)
+                            {
+                                if (match.Scores[0].ID == e.Row && match.Scores[1].ID == e.Column)
+                                {
+                                    match.Scores[0].Points = e.ScorePlayer1;
+                                    match.Scores[1].Points = e.ScorePlayer2;
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                if (match.Scores[0].ID == e.Column && match.Scores[1].ID == e.Row)
+                                {
+                                    match.Scores[0].Points = e.ScorePlayer2;
+                                    match.Scores[1].Points = e.ScorePlayer1;
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                    string filename = string.Empty;
+                    switch (((Models.New.Tournament)_tournament).Groups[_currentGroup].Type)
+                    {
+                        case Tools.GroupType.Girls:
+                            filename = $"girls.{_currentGroup + 1}.json";
+                            break;
+                        case Tools.GroupType.Boys:
+                            filename = $"boys.{_currentGroup - 2}.json";
+                            break;
+                    }
+                    if (!string.IsNullOrWhiteSpace(filename))
+                    {
+                        using StreamWriter sw = new(Path.Combine(_root, _tournament.Name, filename), false, Encoding.UTF8);
+                        sw.Write(System.Text.Json.JsonSerializer.Serialize(((Models.New.Tournament)_tournament).Groups[_currentGroup]));
+                    }
+                    MessageBox.Show("test");
+                }
             }
         }
         private void OpenScore(object sender, MouseButtonEventArgs e)
@@ -114,17 +180,84 @@ namespace AnddeXokoTxapelketa.Controls
             {
                 _ = int.TryParse(matches[0].Groups[1].Value, out int row);
                 _ = int.TryParse(matches[0].Groups[2].Value, out int column);
-                //(_isBoysCurrent ? _tournament.Boys : _tournament.Girls)[_currentGroup].Players[row - 1].Results[column - ((row > column) ? 1 : 2)] = score.Value;
-                //Tools.SaveTournament(_root, _tournament);
-                ScoreDialog scoreDialog = (ScoreDialog)popup.Child;
-                scoreDialog.Init(
-                    row,
-                    (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[row - 1].Name,
-                    score.Value,
-                    column,
-                    (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[column - 1].Name,
-                    ((Score)FindName($"ScoreR{column}C{row}")).Value);
-                popup.IsOpen = true;
+                if (_tournament is Models.Tournament)
+                {
+                    (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[row - 1].Results[column - ((row > column) ? 1 : 2)] = score.Value;
+                    Tools.SaveTournament(_root, (Models.Tournament)_tournament);
+                    ScoreDialog scoreDialog = (ScoreDialog)popup.Child;
+                    scoreDialog.Init(
+                        row,
+                        (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[row - 1].Name,
+                        score.Value,
+                        column,
+                        (_isBoysCurrent ? ((Models.Tournament)_tournament).Boys : ((Models.Tournament)_tournament).Girls)[_currentGroup].Players[column - 1].Name,
+                        ((Score)FindName($"ScoreR{column}C{row}")).Value);
+                    popup.IsOpen = true;
+                }
+                else if (_tournament is Models.New.Tournament)
+                {
+                    int rowPlayerID = 0;
+                    int rowScore = 0;
+                    int columnPlayerID = 0;
+                    int columnScore = 0;
+                    foreach (Models.New.Rotation rotation in ((Models.New.Tournament)_tournament).Groups[_currentGroup].Rotations)
+                    {
+                        foreach (Models.New.Match match in rotation.Matches)
+                        {
+                            if (row < column)
+                            {
+                                if (match.Scores[0].ID == row && match.Scores[1].ID == column)
+                                {
+                                    rowPlayerID = match.Scores[0].ID;
+                                    rowScore = score.Value;
+                                    columnPlayerID = match.Scores[1].ID;
+                                    columnScore = ((Score)FindName($"ScoreR{column}C{row}")).Value;
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                if (match.Scores[0].ID == column && match.Scores[1].ID == row)
+                                {
+                                    rowPlayerID = match.Scores[0].ID;
+                                    rowScore = ((Score)FindName($"ScoreR{column}C{row}")).Value;
+                                    columnPlayerID = match.Scores[1].ID;
+                                    columnScore = score.Value;
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                    ScoreDialog scoreDialog = (ScoreDialog)popup.Child;
+                    scoreDialog.Init(
+                        row,
+                        GetPlayerNameFromID(rowPlayerID),
+                        rowScore,
+                        column,
+                        GetPlayerNameFromID(columnPlayerID),
+                        columnScore);
+                    popup.IsOpen = true;
+                }
+            }
+        }
+        private string GetPlayerNameFromID(int playerIDInScore)
+        {
+            switch (((Models.New.Tournament)_tournament).Groups[_currentGroup].Type)
+            {
+                case Tools.GroupType.Boys:
+                    return ((Models.New.Tournament)_tournament).Boys.FirstOrDefault(c => c.ID == ((Models.New.Tournament)_tournament).Groups[_currentGroup].Players[playerIDInScore - 1]).Name;
+                case Tools.GroupType.Girls:
+                    return ((Models.New.Tournament)_tournament).Girls.FirstOrDefault(c => c.ID == ((Models.New.Tournament)_tournament).Groups[_currentGroup].Players[playerIDInScore - 1]).Name;
+                default:
+                    return string.Empty;
             }
         }
         #endregion
@@ -178,22 +311,12 @@ namespace AnddeXokoTxapelketa.Controls
             TBGroup.Text = group.Name;
             for (int i = 1; i <= 8; i++)
             {
-                if (i <= group.Players.Count)
+                Models.New.Player player = ((Models.New.Tournament)_tournament).Girls.FirstOrDefault(c => c.ID == group.Players[i - 1]);
+                if (player != null && !string.IsNullOrEmpty(player.Name))
                 {
                     EnablePlayer(i);
-                    Models.New.Player player = ((Models.New.Tournament)_tournament).Girls.FirstOrDefault(c => c.ID == group.Players[i - 1]);
                     ((PlayerName)FindName($"PlayerName{i}")).Value = player.Name;
                     ((PlayerLabel)FindName($"PlayerLabel{i}")).Value = player.Name;
-                    //int index = 0;
-                    //for (int j = 1; j <= groups[_currentGroup].Players[i - 1].Results.Count + 1; j++)
-                    //{
-                    //    if (j == i)
-                    //    {
-                    //        continue;
-                    //    }
-                    //    ((Score)FindName($"ScoreR{i}C{j}")).Value = groups[_currentGroup].Players[i - 1].Results[index];
-                    //    index++;
-                    //}
                 }
                 else
                 {
@@ -206,10 +329,17 @@ namespace AnddeXokoTxapelketa.Controls
                 {
                     int id1 = match.Scores[0].ID;
                     int id2 = match.Scores[1].ID;
-                    ((Score)FindName($"ScoreR{id1}C{id2}")).Value = match.Scores[0].Points;
-                    ((Score)FindName($"ScoreR{id2}C{id1}")).Value = match.Scores[1].Points;
+                    Score score = (Score)FindName($"ScoreR{id1}C{id2}");
+                    if (score.CZ.IsEnabled)
+                    {
+                        score.Value = match.Scores[0].Points;
+                    }
+                    score = (Score)FindName($"ScoreR{id2}C{id1}");
+                    if (score.CZ.IsEnabled)
+                    {
+                        score.Value = match.Scores[1].Points;
+                    }
                 }
-
             }
         }
         #endregion
