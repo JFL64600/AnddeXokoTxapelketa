d885558db54990fe5a9bc335e15ed6adbbedab00
Bastien <bmoulia@gmail.com>
2026-03-05 22:47:06 +0100

    feat: implement scoring matrix for tournament groups and update player name retrieval

--- axt/src/app/tournament/tournament.ts
+++ axt/src/app/tournament/tournament.ts
@@ -44,9 +44,29 @@ export class TournamentPage implements OnInit {
     this.boysObservable = objectVal(ref(this.database, this.BoysTableName));
     this.girlsObservable = objectVal(ref(this.database, this.GirlsTableName));
     this.groupsObservable = objectVal(ref(this.database, this.GroupsTableName));
+
     effect(() => {
       if (this.tournament().Groups !== null && this.tournament().Groups.length > 0) {
         console.log('Ongi!!!');
+        this.tournament().Groups.forEach((group) => {
+          const size = group.Players.length;
+          const matrix = Array.from({ length: size }, () => Array(size).fill(0));
+          group.Rotations.forEach((rotation) => {
+            rotation.Matches.forEach((match) => {
+              const player1Index = group.Players.findIndex(
+                (playerID) => playerID === match.Scores[0].ID,
+              );
+              const player2Index = group.Players.findIndex(
+                (playerID) => playerID === match.Scores[1].ID,
+              );
+              if (player1Index !== -1 && player2Index !== -1) {
+                matrix[player1Index][player2Index] = match.Scores[0].Points;
+                matrix[player2Index][player1Index] = match.Scores[1].Points;
+              }
+            });
+          });
+          group.Matrix = matrix;
+        });
       }
     });
   }
@@ -81,13 +101,17 @@ export class TournamentPage implements OnInit {
     });
   }
 
-  public getPlayerNameFromID(group: Group, playerIDInScore: number): string {
+  public getPlayerNameFromID(
+    tournament: Tournament,
+    group: Group,
+    playerIDInScore: number,
+  ): string {
     const playerID = group.Players[playerIDInScore - 1];
     switch (group.Type) {
       case 0:
-        return this.tournament().Girls[playerID - 1].Name;
+        return tournament?.Girls[playerID - 1]?.Name ?? '';
       case 1:
-        return this.tournament().Boys[playerID - 1].Name;
+        return tournament?.Boys[playerID - 1]?.Name ?? '';
       default:
         return '';
     }
